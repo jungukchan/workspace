9e6ce45e3b73abd2565e844455457dd4fc5107bc
jungukchan <wkchan2254@gmail.com>
2025-03-25 15:43:31 +0900

    fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,7 +12,11 @@ function login (){
         psword : psword.value,
     }
 
-    console.log(req);
-
-    //fetch();
+    fetch("/login", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(req),
+    });
 }
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,8 +7,8 @@ const ctrl = require("./home.ctrl");
 
 //-- Controller --
 router.get("/", ctrl.home);
-
 router.get("/login", ctrl.login);
+
 
 //외부로 모듈을 사용할 수 있도록 처리하는 로직
 module.exports = router; 
